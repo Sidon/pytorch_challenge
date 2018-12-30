13f0e616ba973c9313129b78be88489510ae92f2
Sidon Duarte <sidoncd@gmail.com>
2018-12-30 18:30:51 -0200

    Removendo __fc

--- torch_helper.py
+++ torch_helper.py
@@ -12,39 +12,56 @@ import torch.optim as optim
 
 
 class MyNet:
-    def __init__(self, trained_name, fc_layers = [512, 256], dropout=0.2, out_features = 102):
+    def __init__(self, trained_name, fc_layers = None, out_features = 102):
         # Additional variables will be used to track training progress for easier re-loads, plotting, etc.
 
-        self.fc_layers = fc_layers
-        self.dropout = dropout
-        self.out_features = out_features
-        self.__trained_name = trained_name
 
-        self.__tracking = {}
-        self.__tracking['last_epoch'] = 0
-        self.__tracking['min_loss'] =  99
-        self.__tracking['learn_rates'] = []
-        self.__tracking['validation_loss'] = []
-        self.__tracking['training_loss'] = []
-        self.__tracking['accuracy_rate'] = []
-        self.__tracking['trained_name'] = trained_name
-        self.__tracking['los_name'] = None
-        self.__tracking['optmizer_name'] = None
+        # self.__tracking = {}
+        # self.__tracking['last_epoch'] = 0
+        # self.__tracking['min_loss'] =  99
+        # self.__tracking['learn_rates'] = []
+        # self.__tracking['validation_loss'] = []
+        # self.__tracking['training_loss'] = []
+        # self.__tracking['accuracy_rate'] = []
+        # self.__tracking['trained_name'] = trained_name
+        # self.__tracking['los_name'] = None
+        # self.__tracking['optmizer_name'] = None
 
         self.__trained_models = {
-            'ResNet152': {'size': 2048, 'model': models.resnet152},
-            'Inception': {'size': 2048, 'model': models.inception_v3},
-            'DenseNet161': {'size': 2208, 'model': models.densenet161}
+            'vgg11': {'model': models.vgg11, 'change_model': self.__vgg},
+            'vgg11_bn': {'model': models.vgg11_bn, 'change_model': self.__vgg},
+            'vgg13': {'model': models.vgg13}, 'change_model': self.__vgg,
+            'vgg13_bn': {'model': models.vgg13_bn}, 'change_model': self.__vgg,
+            'vgg16': {'model': models.vgg16, 'change_model': self.__vgg},
+            'vgg16_bn': {'model': models.vgg16_bn, 'change_model': self.__vgg},
+            'vgg19': {'model': models.vgg19, 'change_model': self.__vgg},
+            'vgg19_bn': {'model': models.vgg19_bn, 'change_model': self.__vgg},
+            'resnet101': {'model': models.resnet101, 'change_model': self.__resnet},
+            'resnet152': {'model': models.resnet152, 'change_model': self.__resnet},
+            'resnet18': {'model': models.resnet18, 'change_model': self.__resnet},
+            'resnet34': {'model': models.resnet34, 'change_model': self.__resnet},
+            'resnet50': {'model': models.resnet50, 'change_model': self.__resnet},
+            'densenet121': {'model': models.densenet121, 'change_model': self.__densenet},
+            'densenet161': {'model': models.densenet161, 'change_model': self.__densenet},
+            'densenet169': {'model': models.densenet169, 'change_model': self.__densenet},
+            'densenet201': {'model': models.densenet201, 'change_model': self.__densenet},
+            'inception': {'model': models.inception_v3, 'change_model': self.__inception},
         }
 
         # Import trained model
-        self.__trained_model = self.__trained_models[self.__trained_name]['model'](pretrained=True)
+        model = self.__trained_models[trained_name]['model'](pretrained=True)
+        #
+        # print( 'Name===>', self.__trained_name,)
+        # print( 'model===>', self.__trained_models[self.__trained_name])
+        # print( 'change===>', self.__trained_models['change_model'])
+
+        self.__trained_model = self.__trained_models[trained_name]['change_model'](model, fc_layers, out_features)
 
         # Create the full connected layer
-        self._fc = self.__fc(self.__trained_models[trained_name]['size'], fc_layers, self.dropout, self.out_features)
+        #self._fc = self.__fc(self.__trained_models[trained_name]['size'], fc_layers, self.dropout, self.out_features)
 
         # Replace fc in trained model
-        self.__trained_model.fc = self.fc
+        # self.__trained_model.fc = self.fc
 
     def __fc(self, in_features, hidden_features = [512, 256], dropout=0.2, out_features = 102 ):
         fc = nn.Sequential()  # type: object
@@ -58,6 +75,47 @@ class MyNet:
             n+=1
         fc.add_module('out', nn.Linear(in_features, out_features))
         return fc
+
+
+    def __vgg(self, model, fc=None, out_features=102):
+        if not fc is None:
+            model.classifier = fc
+        else:
+            model.classifier[6].out_features = out_features
+        return model
+
+    def __densenet(self, model, fc=None, out_features=102):
+        if not fc is None:
+            model.classifier = clf
+        else:
+            model.classifier.out_features = out_features
+        return model
+
+    def __inception(self, model, fc=None, out_features=102):
+        if not fc is None:
+            model.fc = fc
+        else:
+            model.fc.out_features = out_features
+        return model
+
+
+    def __squeeznet(self, model, fc=None, out_features=102):
+        if not fc is None:
+            model.fc = fc
+        else:
+            model.fc.out_features = out_features
+        return model
+
+
+    def __resnet(self, model, fc=None, out_features=102):
+
+        print('fc==>', fc)
+
+        if not fc is None:
+            model.fc = fc
+        else:
+            model.fc.out_features = out_features
+        return model
 
     @property
     def fc(self):
@@ -73,7 +131,6 @@ class MyNet:
 
     def __import_trained_model(self):
         return  self.__trained_models[self.__trained_name]['model'](pretrained=True)
-
 
 
 class Transforms:
@@ -93,12 +150,10 @@ class Transforms:
         lst_compose = augment + [tv_transforms.ToTensor(), tv_transforms.Normalize( normalize[0], normalize[1]) ]
         return tv_transforms.Compose(lst_compose)
 
-
 class Dataset:
     @staticmethod
     def dataset(data_dir, transform):
         return datasets.ImageFolder(data_dir, transform = transform)
-
 
 class Loaders:
     num_workers=0
@@ -107,5 +162,70 @@ class Loaders:
     def loader(cls, dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers):
         return torch.utils.data.DataLoader(dataset, shuffle=shuffle, batch_size = batch_size,
                                            num_workers = num_workers)
+
+
+# class TrainModel:
+#     def __init__(self, model, train_loader, validation_loader, criterion, optimizer, colab_kernel=False, epochs=3, print_every=30):
+#         self.model = model
+#         self.train_loader = train_loader
+#         self.validation_loader = validation_loader
+#         self.criterion = criterion
+#         self.optimizer = optimizer
+#         self.epochs = epochs
+#         self.print_every = print_every
+#         self.colab_kernel = colab_kernel
+#
+#     def validation(self, _model, _data_loader, criterion):
+#         valid_correct = 0
+#         valid_loss = 0
+#
+#         for images, labels in (iter(_data_loader)):
+#             if self.colab_kernel:
+#                 images, labels = images.to('cuda'), labels.to('cuda')
+#
+#             # Forward pass
+#             output = _model.forward(images)
+#
+#             # Track loss
+#             valid_loss += loss.item()
+#
+#
+#     steps = 0
+#         running_loss = 0
+#         for e in range(epochs):
+#             # Model in training mode, dropout is on
+#             model.train()
+#             for images, labels in train_loader:
+#                 steps += 1
+#
+#                 # Flatten images into a 784 long vector
+#                 images.resize_(images.size()[0], 784)
+#
+#                 optimizer.zero_grad()
+#
+#                 output = model.forward(images)
+#                 loss = criterion(output, labels)
+#                 loss.backward()
+#                 optimizer.step()
+#
+#                 running_loss += loss.item()
+#
+#                 if steps % print_every == 0:
+#                     # Model in inference mode, dropout is off
+#                     model.eval()
+#
+#                     # Turn off gradients for validation, will speed up inference
+#                     with torch.no_grad():
+#                         test_loss, accuracy = validation(model, testloader, criterion)
+#
+#                     print("Epoch: {}/{}.. ".format(e + 1, epochs),
+#                           "Training Loss: {:.3f}.. ".format(running_loss / print_every),
+#                           "Test Loss: {:.3f}.. ".format(test_loss / len(testloader)),
+#                           "Test Accuracy: {:.3f}".format(accuracy / len(testloader)))
+#
+#                     running_loss = 0
+#
+#                     # Make sure dropout and grads are on for training
+#                     model.train()
 
 
--- util_helper.py
+++ util_helper.py
@@ -9,3 +9,21 @@ def rnd_classes(batch, batch_classes,  dict_classes, k=5):
     return imgs, labels
 
 
+
+  # def get_vgg(self):
+  #           vggs = {}
+  #           print('vgg11')
+  #           vggs['vgg11'] = models.vgg11(pretrained=True)
+  #           vggs['vgg11_bn'] = models.vgg11_bn(pretrained=True)
+  #           vggs['vgg13'] = models.vgg13(pretrained=True)
+  #           vggs['vgg13_bn'] = models.vgg13_bn(pretrained=True)
+  #           vggs['vgg16'] = models.vgg16(pretrained=True)
+  #           vggs['vgg16_bn'] = models.vgg16_bn(pretrained=True)
+  #           vggs['vgg19'] = models.vgg19(pretrained=True)
+  #           vggs['vgg19_bn'] = models.vgg19_bn(pretrained=True)
+  #           return vggs
+  #
+  #   def get_densenet(self):
+  #       ds169 = models.densenet169(pretrained=True)
+  #       ds201 = models.densenet201(pretrained=True)
+  #       print(ds169.classifier, ds201.classifier, sep='\n')
